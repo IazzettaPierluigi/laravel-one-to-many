1c080859cc19308b1f7d8330336fe2ecfaf93d4e
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-04-03 17:42:12 +0200

    show of type

--- resources/views/pages/projects/show.blade.php
+++ resources/views/pages/projects/show.blade.php
@@ -11,6 +11,9 @@
         <img src="{{ asset('/storage/' . $project->img) }}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">{{ $project->title }}</h5>
+          <p class="text-danger">
+            {{ $project->type ? $project->type->name : 'Questo progetto non ha un type' }}
+          </p>
           <p class="card-text">{{$project->description}}</p>
         </div>
       </div>
