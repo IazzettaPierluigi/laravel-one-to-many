f521bc336f3f2c5f70307a279bfd1d48deb299ed
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-04-03 14:07:22 +0200

    belongs to and hasmany

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Str;
 
 class Project extends Model
@@ -11,18 +12,22 @@ class Project extends Model
     use HasFactory;
 
     protected $fillable = [
-
         'id',
         'title',
         'img',
         'description',
         'software',
         'slug'
-
     ];
 
     public static function generateSlug($title)
     {
         return Str::slug($title, '-');
+    }
+
+    // Un progetto può avere una sola categoria, questa è la relazione "one-to-many"
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
     }
 }
